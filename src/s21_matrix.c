16034f5814d374c4a4923b614aa084151b743c2b
Tamela Bears <tamelabe@un-c5.kzn.21-school.ru>
2022-07-20 14:13:07 +0300

    Finish

--- src/s21_matrix.c
+++ src/s21_matrix.c
@@ -1,71 +1,33 @@
 #include "s21_matrix.h"
-
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-// BASIC FUNCTIONS
-int s21_create_matrix(int rows, int columns, matrix_t *result);     // done
-void s21_remove_matrix(matrix_t *A);                                // done
-int s21_eq_matrix(matrix_t *A, matrix_t *B);                        // done
-int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);     // done
-int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);     // done
-int s21_mult_number(matrix_t *A, double number, matrix_t *result);  // done
-int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);    // done
-int s21_transpose(matrix_t *A, matrix_t *result);                   // done
-int s21_calc_complements(matrix_t *A, matrix_t *result);
-int s21_determinant(matrix_t *A, double *result);
-int s21_inverse_matrix(matrix_t *A, matrix_t *result);
-
-// ADDITIONAL FUNCTIONS
-int s21_check_mex(matrix_t *A);
-int s21_check_mrc(matrix_t *A);
-int s21_check_msz(matrix_t *A, matrix_t *B);
-void s21_rm_rc(matrix_t *BF, int nj, matrix_t *AF);
-
+/*
 int main() {
-  int rows = 3, columns = 3;
-  double det = 0;
-  int cnt_test = 0;
-  matrix_t A;  //, B, result;
-  s21_create_matrix(3, 3, &A);
-  // s21_create_matrix(2, 3, &B);
-  printf("Fill A matrix: ");
-  for (int i = 0; i < A.rows; i++) {
-    for (int j = 0; j < A.columns; j++) {
-      A.matrix[i][j] = cnt_test;
-      cnt_test++;
-      // scanf("%lf", &A.matrix[i][j]);
+    double count = 1.0;
+    matrix_t A;
+    matrix_t B;
+    matrix_t C;
+    s21_create_matrix(2, 3, &A);
+    s21_create_matrix(3, 2, &B);
+    for (int i = 0; i < A.rows; i++) {
+        for (int j = 0 ; j < A.columns; j++) {
+            A.matrix[i][j] = count;
+            B.matrix[j][i] = count;
+            count = count + 1.0;
+        }
     }
-  }
-  /*
-  printf("Fill B matrix: ");
-  for (int i = 0; i < B.rows; i++) {
-    for (int j = 0; j < B.columns; j++) {
-      scanf("%lf", &B.matrix[i][j]);
+
+    int res = s21_transpose(&A, &C);
+    for (int i = 0; i < A.rows; i++) {
+      for (int j = 0; j < A.columns; j++) {
+          printf("[%d][%d] A = %.0lf B = %.0lf C = %.0lf\n", i, j, A.matrix[i][j], B.matrix[i][j], C.matrix[i][j]);
+      }
     }
-  }
-  */
-  // s21_mult_matrix(&A, &B, &result);
-  // s21_remove_matrix(&A);
-  // s21_remove_matrix(&B);
-  // printf("Mult is: \n");
-  // for (int i = 0; i < columns; i++) {
-  //   for (int j = 0; j < rows; j++) {
-  //     printf("%-2.0lf", result.matrix[i][j]);
-  //     if (j == columns - 1 && i < rows - 1) {
-  //       putchar('\n');
-  //     } else if (j != columns - 1) {
-  //       putchar(' ');
-  //     }
-  //   }
-  // }
-  s21_determinant(&A, &det);
-  s21_remove_matrix(&A);
-  printf("Determinant is: %.0lf\n", det);
+    int res_eq = s21_eq_matrix(&B, &C);
+    s21_remove_matrix(&A);
+    s21_remove_matrix(&B);
+    s21_remove_matrix(&C);
   return 0;
 }
-
+*/
 int s21_create_matrix(int rows, int columns, matrix_t *result) {
   int res_code = INCRT_MTRX;
   result->matrix = NULL;
@@ -100,8 +62,8 @@ int s21_eq_matrix(matrix_t *A, matrix_t *B) {
   if (s21_check_mex(A) && s21_check_mex(B) && s21_check_msz(A, B)) {
     res_code = SUCCESS;
     for (int i = 0; i < A->rows; i++) {
-      for (int j = 0; j < A->columns; i++) {
-        if (fabs(A->matrix[i][j] - B->matrix[i][j] > 1e-7)) {
+      for (int j = 0; j < A->columns; j++) {
+        if (fabs(A->matrix[i][j] - B->matrix[i][j]) > 1e-7) {
           res_code--;
         }
       }
@@ -115,14 +77,12 @@ int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
   int res_code = OK;
   if (!s21_check_mex(A) || !s21_check_mex(B)) {
     res_code = INCRT_MTRX;
-  } else {
-    if (!s21_check_msz(A, B)) {
+  } else if (!s21_check_msz(A, B)) {
       res_code = CALC_ERROR;
-    } else if (!(res_code = s21_create_matrix(A->rows, A->columns, result))) {
-      for (int i = 0; i < A->rows; i++) {
-        for (int j; j < A->columns; j++) {
-          result->matrix[i][j] = A->matrix[i][j] + B->matrix[i][j];
-        }
+  } else if (!(res_code = s21_create_matrix(A->rows, A->columns, result))) {
+    for (int i = 0; i < A->rows; i++) {
+      for (int j = 0; j < A->columns; j++) {
+        result->matrix[i][j] = A->matrix[i][j] + B->matrix[i][j];
       }
     }
   }
@@ -138,7 +98,7 @@ int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
       res_code = CALC_ERROR;
     } else if (!(res_code = s21_create_matrix(A->rows, A->columns, result))) {
       for (int i = 0; i < A->rows; i++) {
-        for (int j; j < A->columns; j++) {
+        for (int j = 0; j < A->columns; j++) {
           result->matrix[i][j] = A->matrix[i][j] - B->matrix[i][j];
         }
       }
@@ -153,7 +113,7 @@ int s21_mult_number(matrix_t *A, double number, matrix_t *result) {
     res_code = INCRT_MTRX;
   } else if (!(res_code = s21_create_matrix(A->rows, A->columns, result))) {
     for (int i = 0; i < A->rows; i++) {
-      for (int j; j < A->columns; j++) {
+      for (int j = 0; j < A->columns; j++) {
         result->matrix[i][j] = A->matrix[i][j] * number;
       }
     }
@@ -176,7 +136,7 @@ int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result) {
       }
     }
   } else {
-    res_code == CALC_ERROR;
+    res_code = CALC_ERROR;
   }
   return res_code;
 }
@@ -188,7 +148,26 @@ int s21_transpose(matrix_t *A, matrix_t *result) {
   } else if (!(res_code = s21_create_matrix(A->columns, A->rows, result))) {
     for (int i = 0; i < A->rows; i++) {
       for (int j = 0; j < A->columns; j++) {
-        result->matrix[i][j] = A->matrix[j][i];
+        result->matrix[j][i] = A->matrix[i][j];
+      }
+    }
+  }
+  return res_code;
+}
+
+int s21_calc_complements(matrix_t *A, matrix_t *result) {
+  int res_code = OK;
+  if (!s21_check_mex(A) || !s21_check_mrc(A)) {
+    res_code = INCRT_MTRX;
+  } else if (A->rows != A->columns) {
+    res_code = CALC_ERROR;
+  } else {
+    if (!(res_code = s21_create_matrix(A->rows, A->columns, result))) {
+      res_code = s21_convert_to_minor(A, result);
+      for (int i = 0; i < A->rows; i++) {
+        for (int j = 0; j < A->columns; j++) {
+          result->matrix[i][j] *= pow(-1, i + j);
+        }
       }
     }
   }
@@ -197,7 +176,7 @@ int s21_transpose(matrix_t *A, matrix_t *result) {
 
 int s21_determinant(matrix_t *A, double *result) {
   int res_code = OK;
-  double degree = 1.0;
+  double degree = 1.0, nres = 0;
   if (!s21_check_mex(A) || !s21_check_mrc(A)) {
     res_code = INCRT_MTRX;
   } else if (A->rows != A->columns) {
@@ -206,28 +185,18 @@ int s21_determinant(matrix_t *A, double *result) {
     if (A->rows == 1) {
       *result = A->matrix[0][0];
     } else if (A->rows == 2) {
-      *result == (A->matrix[0][0] * A->matrix[1][1]) -
-                     (A->matrix[0][1] * A->matrix[1][0]);
+      *result = (A->matrix[0][0] * A->matrix[1][1]) -
+                (A->matrix[0][1] * A->matrix[1][0]);
     } else {
       matrix_t Rmatrix;
       if (!(res_code =
                 s21_create_matrix(A->columns - 1, A->rows - 1, &Rmatrix))) {
         for (int j = 0; j < A->rows; j++) {
-          s21_rm_rc(A, j, &Rmatrix);
-          *result +=
-              (degree * A->matrix[0][j] + s21_determinant(&Rmatrix, result));
+          s21_rm_rc(A, 0, j, &Rmatrix);
+          s21_determinant(&Rmatrix, &nres);
+          *result += degree * A->matrix[0][j] * nres;
           degree = -degree;
-          printf("result = %.0lf\n", *result);
-        }
-        for (int i = 0; i < Rmatrix.columns; i++) {
-          for (int j = 0; j < Rmatrix.rows; j++) {
-            printf("%-2.0lf", Rmatrix.matrix[i][j]);
-            if (j == Rmatrix.rows - 1 && i < Rmatrix.columns - 1) {
-              putchar('\n');
-            } else if (j != Rmatrix.columns - 1) {
-              putchar(' ');
-            }
-          }
+          nres = 0;
         }
         s21_remove_matrix(&Rmatrix);
       }
@@ -236,18 +205,59 @@ int s21_determinant(matrix_t *A, double *result) {
   return res_code;
 }
 
-void s21_rm_rc(matrix_t *BF, int nj, matrix_t *AF) {
+int s21_convert_to_minor(matrix_t *A, matrix_t *MM) {
+  int res_code = OK;
+  matrix_t buf;
+  if (!(res_code = s21_create_matrix(A->rows - 1, A->columns - 1, &buf))) {
+    for (int i = 0; i < A->rows; i++) {
+      for (int j = 0; j < A->columns; j++) {
+        double det = 0.0;
+        s21_rm_rc(A, i, j, &buf);
+        s21_determinant(&buf, &det);
+        MM->matrix[i][j] = det;
+      }
+    }
+    s21_remove_matrix(&buf);
+  }
+  return res_code;
+}
+
+int s21_inverse_matrix(matrix_t *A, matrix_t *result) {
+  int res_code = OK;
+  if (!s21_check_mex(A) || !s21_check_mrc(A)) {
+    res_code = INCRT_MTRX;
+  } else if (A->rows != A->columns) {
+    res_code = CALC_ERROR;
+  } else {
+    double det = 0;
+    s21_determinant(A, &det);
+    if (fabs(det) > 1e-7) {
+      matrix_t tempM1, tempM2;
+      s21_calc_complements(A, &tempM1);
+      s21_transpose(&tempM1, &tempM2);
+      s21_remove_matrix(&tempM1);
+      s21_mult_number(&tempM2, 1.0 / det, result);
+      s21_remove_matrix(&tempM2);
+    } else {
+      res_code = CALC_ERROR;
+    }
+  }
+  return res_code;
+}
+
+void s21_rm_rc(matrix_t *BF, int ni, int nj, matrix_t *AF) {
   int irow = 0, icol = 0;
-  for (int i = 0; i < BF->rows - 1; i++) {
-    if (!i) {
+  for (int i = 0; i < AF->rows; i++) {
+    if (i == ni) {
       irow = 1;
     }
-    for (int j = 0; j < BF->rows - 1; j++) {
+    for (int j = 0; j < AF->columns; j++) {
       if (j == nj) {
         icol = 1;
       }
       AF->matrix[i][j] = BF->matrix[i + irow][j + icol];
     }
+    icol = 0;
   }
 }
 
--- src/s21_matrix.h
+++ src/s21_matrix.h
@@ -1,5 +1,10 @@
 #ifndef SRC_S21_MATRIX_H_
 #define SRC_S21_MATRIX_H_
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #define OK 0
 #define INCRT_MTRX 1
 #define CALC_ERROR 2
@@ -7,11 +12,28 @@
 #define FAILURE 0
 
 typedef struct matrix_struct {
-    double** matrix;
-    int rows;
-    int columns;
+  double **matrix;
+  int rows;
+  int columns;
 } matrix_t;
 
+// BASIC FUNCTIONS
+int s21_create_matrix(int rows, int columns, matrix_t *result);
+void s21_remove_matrix(matrix_t *A);
+int s21_eq_matrix(matrix_t *A, matrix_t *B);
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_mult_number(matrix_t *A, double number, matrix_t *result);
+int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+int s21_transpose(matrix_t *A, matrix_t *result);
+int s21_calc_complements(matrix_t *A, matrix_t *result);
+int s21_determinant(matrix_t *A, double *result);
+int s21_inverse_matrix(matrix_t *A, matrix_t *result);
+// ADDITIONAL FUNCTIONS
+int s21_check_mex(matrix_t *A);
+int s21_check_mrc(matrix_t *A);
+int s21_check_msz(matrix_t *A, matrix_t *B);
+void s21_rm_rc(matrix_t *BF, int ni, int nj, matrix_t *AF);
+int s21_convert_to_minor(matrix_t *A, matrix_t *MM);
 
 #endif  // SRC_S21_MATRIX_H_
-
